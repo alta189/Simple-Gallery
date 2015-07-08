759ffe1e21d11ad74fd50c776f934620b784ef75
Stephen Williams <me@alta189.com>
2015-07-08 00:33:08 -0400

    SendGrid for email management

--- src/main/java/com/alta189/simple/gallery/ServerSettings.java
+++ src/main/java/com/alta189/simple/gallery/ServerSettings.java
@@ -4,15 +4,23 @@ import org.aeonbits.owner.Config;
 
 @Config.Sources("file:server.properties")
 public interface ServerSettings extends Config {
+	@Key("server.port")
     @DefaultValue("80")
-    @Key("server.port")
     int port();
 
+	@Key("server.timezone")
     @DefaultValue("EST5EDT")
-    @Key("server.timezone")
     String timezone();
 
     @Key("dev")
     @DefaultValue("false")
     boolean dev();
+
+    @Key("email.address")
+	@DefaultValue("")
+	String emailAddress();
+
+	@Key("email.sendgrid.apikey")
+	@DefaultValue("")
+	String sendgridApiKey();
 }
